01895cc9e9bdcd5629606b53b58dbabb4d2e576e
Onur Kudret <onurkudrt31@gmail.com>
2024-10-03 22:45:05 +0300

    Tailwind.css setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      // Add the paths to all of your template files in your tailwind.config.js file.
+      "./assets/**/*.js",
+      "./templates/**/*.html.twig"
+      // Add the @tailwind directives for each of Tailwind’s layers to your ./assets/styles/app.css file.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
