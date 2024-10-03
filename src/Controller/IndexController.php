a76c72c3e7ed9916866d1a8fb4cf1466fcae625e
Alper Uyanik <alperuyanik4342@gmail.com>
2024-10-03 17:56:54 +0300

    first commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+            'mesaj' => [
+                'test' => 'test1',
+                'test2' => 'test2'
+            ]
+        ]
+    );
+    }
+}
