6b5630de8540f38adce6595a5275e7bb1d856b75
Onur Kudret <onurkudrt31@gmail.com>
2024-10-08 23:09:32 +0300

    Merge from test

--- assets/security/VueAuthenticator.js
+++ assets/security/VueAuthenticator.js
@@ -1,4 +1,4 @@
-import {login} from "../http/authentication/authentication";
+import { login, checkAuthentication } from "../http/authentication/authentication";
 
 /**
  * @param {string} identifier
@@ -8,15 +8,42 @@ export const authenticate = function (identifier, password) {
     login({
         email: identifier,
         password: password
-    }).then(
-        response=> console.log(response)
-    );
+    }).then(response => {
+
+        if (!response.ok) {
+            console.error("Hata:", response.error);
+            alert("Şifre veya email yanlış.");
+        } else {
+            const token = response.result.accessToken;
+            if (token && typeof token === 'string') {
+                sessionStorage.setItem("authToken", token);
+                alert("Giriş başarılı!");
+                isAuthenticated();
+            } else {
+                alert('Token yok');
+            }
+        }
+    }).catch(error => {
+        console.error("İstek hatası:", error);
+        alert("Giriş işlemi sırasında bir hata oluştu.");
+    });
 };
 
-export const unauthenticated = function (){
+export const unauthenticated = function () {
 
 };
 
-export const isAuthenticated = function (){
-    return false;
-}
+export const isAuthenticated = function () {
+    const token = sessionStorage.getItem("authToken");
+
+    if (!token)
+        return false;
+
+    checkAuthentication({
+        authToken: token
+    }).then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.error(error);
+    });
+};
