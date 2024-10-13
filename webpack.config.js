a0ea6b89cb4b053e8e60a5c579661cf9bd998273
Onur Kudret <onurkudrt31@gmail.com>
2024-10-13 20:30:35 +0300

    Webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -124,7 +124,7 @@ Encore
  webpack.config.js dosyanıza şunları ekleyin:
  */
     .addPlugin(new webpack.DefinePlugin({
-        __VUE_OPTIONS_API__: JSON.stringify(true), // veya false
+        __VUE_OPTIONS_API__: JSON.stringify(true),
         __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
     }))
