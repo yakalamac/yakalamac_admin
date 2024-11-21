e8e709a45d2f7ab66307877ec7e1790e979a49c5
Kıvanç Hançerli <kivanchancerliv@gmail.com>
2024-11-21 23:18:05 +0300

    changelogs module added #1

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -24,6 +24,10 @@ use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpClient\HttpClient;
+use App\Entity\Changelog;
+use App\Form\ChangelogType;
+use App\Repository\ChangelogRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AdminController extends AbstractController
 {
@@ -36,7 +40,7 @@ class AdminController extends AbstractController
     private $clientFactory;
     private $auditLogRepository;
     private $elasticUrl;
-
+    private ChangelogRepository $changelogRepository;
     /**
      *
      * @param HttpClientInterface $httpClient
@@ -44,7 +48,7 @@ class AdminController extends AbstractController
      * @param ApiController $apiController
      * @param PlaceService $service
      */
-    public function __construct(HttpClientInterface $httpClient, UserService $userService, ApiController $apiController, PlaceService $service, ClientFactory $clientFactory, AuditLogRepository $auditLogRepository, string $elasticUrl)
+    public function __construct(HttpClientInterface $httpClient, UserService $userService, ApiController $apiController, PlaceService $service, ClientFactory $clientFactory, ChangelogRepository $changelogRepository,AuditLogRepository $auditLogRepository, string $elasticUrl)
     {
         $this->httpClient = $httpClient;
         $this->userService = $userService;
@@ -53,6 +57,7 @@ class AdminController extends AbstractController
         $this->clientFactory = $clientFactory;
         $this->auditLogRepository = $auditLogRepository;
         $this->elasticUrl = $elasticUrl;
+        $this->changelogRepository = $changelogRepository;
     }
 
     #[Route('/login', name: 'login_page')]
@@ -62,7 +67,7 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin', name: 'admin_dashboard')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         try {
             $query = [
@@ -91,12 +96,61 @@ class AdminController extends AbstractController
 
         $todayOperations = $this->auditLogRepository->countTodayOperations($actions, $entityTypePrefix);
         $todayOperationsProducts = $this->auditLogRepository->countTodayOperations($actions, $entityTypePrefixP);
+        $seenChangelogIds = $request->cookies->get('seen_changelogs', '');
+        $seenChangelogIds = $seenChangelogIds ? explode(',', $seenChangelogIds) : [];
 
+        $unseenChangelogs = $this->changelogRepository->findUnseenChangelogs($seenChangelogIds);
         return $this->render('admin/pages/dashboard.html.twig', [
             'totalProduct' => $totalProduct,
             'totalBusinesses' => $totalBusinesses,
             'todayOperations' => $todayOperations,
             'todayOperationsProducts' => $todayOperationsProducts,
+            'unseenChangelogs' => $unseenChangelogs,
         ]);
+    }
+
+    #[Route('/admin/changelogs', name: 'admin_changelog_index')]
+    public function changelogs(Request $request, EntityManagerInterface $em, ChangelogRepository $changelogRepository): Response
+    {
+        // $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $changelog = new Changelog();
+        $form = $this->createForm(ChangelogType::class, $changelog);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $changelog->setCreatedAt(new \DateTime());
+
+            $em->persist($changelog);
+            $em->flush();
+
+            $this->addFlash('success', 'Güncelleme notu başarıyla eklendi.');
+
+            return $this->redirectToRoute('admin_changelog_index');
+        }
+
+        $changelogs = $changelogRepository->findBy([], ['createdAt' => 'DESC']);
+
+        return $this->render('admin/pages/system/change-logs.html.twig', [
+            'form' => $form->createView(),
+            'changelogs' => $changelogs,
+        ]);
+    }
+    #[Route('/admin/changelogs/{id}/delete', name: 'admin_changelog_delete', methods: ['POST'])]
+    public function deleteChangelog(Request $request, Changelog $changelog, EntityManagerInterface $em): Response
+    {
+        // $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        if ($this->isCsrfTokenValid('delete-changelog' . $changelog->getId(), $request->request->get('_token'))) {
+            $em->remove($changelog);
+            $em->flush();
+
+            $this->addFlash('success', 'Güncelleme notu başarıyla silindi.');
+        } else {
+            $this->addFlash('error', 'Güncelleme notu silinirken bir hata oluştu.');
+        }
+
+        return $this->redirectToRoute('admin_changelog_index');
     }
 }
---
+++ src/Repository/ChangelogRepository.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Changelog;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use DateTime;
+use DateInterval;
+/**
+ * @extends ServiceEntityRepository<Changelog>
+ */
+class ChangelogRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Changelog::class);
+    }
+
+    /**
+     * Kullanıcının görmediği güncelleme notlarını getirir
+     *
+     * @param array $seenChangelogIds
+     * @return Changelog[]
+     */
+    public function findUnseenChangelogs(array $seenChangelogIds): array
+    {
+        $thirtyDaysAgo = new DateTime();
+        $thirtyDaysAgo->sub(new DateInterval('P30D'));
+
+        $qb = $this->createQueryBuilder('c')
+            ->where('c.createdAt >= :thirtyDaysAgo')
+            ->setParameter('thirtyDaysAgo', $thirtyDaysAgo)
+            ->orderBy('c.createdAt', 'DESC');
+
+        if (!empty($seenChangelogIds)) {
+            $qb->andWhere('c.id NOT IN (:seenIds)')
+               ->setParameter('seenIds', $seenChangelogIds);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
+//    /**
+//     * @return Changelog[] Returns an array of Changelog objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Changelog
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
