9f5bfc8e0b6e1fc28206ebe8e157b5f500364846
bariskudret <bariskudret31@gmail.com>
2025-04-14 01:05:37 +0300

    feat: new controller function added for detail page

--- src/Controller/Admin/Crud/DictionaryController.php
+++ src/Controller/Admin/Crud/DictionaryController.php
@@ -6,12 +6,14 @@
 
 namespace App\Controller\Admin\Crud;
 
+use App\Client\YakalaApiClient;
 use App\Controller\Abstract\BaseController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DictionaryController extends BaseController
 {
+    public function __construct(private readonly YakalaApiClient $client){}
     /**
      * @return Response
      */
@@ -19,5 +21,14 @@ class DictionaryController extends BaseController
     public function index(): Response
     {
         return $this->render('admin/pages/dictionary/index.html.twig');
+    }
+
+    #[Route('/admin/dictionary/detail/{id}', name: 'app_dictionary_detail')]
+    public function detail(string $id): Response{
+        $response = $this->client->get("dictionaries/$id");
+        //dd($response);
+        return  $this->render('admin/pages/dictionary/edit.html.twig', [
+            'dictionary' => $this->client->toArray($response)
+        ]);
     }
 }
