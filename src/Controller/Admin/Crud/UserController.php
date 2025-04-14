b06897281582c7593d7665bab59e46f2af9e0740
bariskudret <bariskudret31@gmail.com>
2025-04-14 15:33:20 +0300

    no changed

--- src/Controller/Admin/Crud/UserController.php
+++ src/Controller/Admin/Crud/UserController.php
@@ -6,6 +6,7 @@
 
 namespace App\Controller\Admin\Crud;
 
+use App\Client\YakalaApiClient;
 use App\Controller\Abstract\BaseController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,10 +16,22 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ADMIN_USER_EDITOR')]
 class UserController extends BaseController
 {
+    public function __construct(private readonly YakalaApiClient $client)
+    {
+    }
     #[Route('/admin/users', name: 'users')]
-
     public function index(Request $request): Response
     {
         return $this->render('admin/pages/user/user.html.twig');
+    }
+
+    #[Route('/admin/users/detail/{id}', name: 'user_detail')]
+    public function detail(Request $request, string $id): Response
+    {
+        $response = $this->client->get("users/$id");
+        //throw new \Exception('fdgg');
+        return $this->render('admin/pages/user/edit.html.twig', [
+            'user' => $this->client->toArray($response),
+        ]);
     }
 }
