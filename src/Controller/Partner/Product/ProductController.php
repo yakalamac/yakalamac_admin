205197def736c48e9a1fa269ac88a7f1d1c7a9be
Onur Kudret <onurkudrt31@gmail.com>
2025-04-27 21:52:49 +0300

    feat: place description and choosen field

---
+++ src/Controller/Partner/Product/ProductController.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @author Onur Kudret
+ * @version 1.0.0
+ */
+
+namespace App\Controller\Partner\Crud\Product;
+
+use App\Client\YakalaApiClient;
+use App\Controller\Abstract\BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Throwable;
+
+#[Route('/partner/products')]
+class ProductController extends BaseController
+{
+    /**
+     * @param YakalaApiClient $client
+     */
+    public function __construct(private readonly YakalaApiClient $client) {}
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    #[Route(name: 'partner_product_list', methods: ['GET'])]
+    public function index(Request $request): Response
+    {
+        return $this->render('partner/layouts/product/index.html.twig');
+    }
+
+    /**
+     * @return Response
+     * @throws Throwable
+     */
+    #[Route('/add', name: 'partner_product_add', methods: ['GET'])]
+    public function add(): Response
+    {
+        return $this->render('partner/layouts/product/add.html.twig');
+    }
+
+    /**
+     * @param string $id
+     * @return Response
+     * @throws Throwable
+     */
+    #[Route('/{id}', name: 'partner_product_edit', methods: ['GET'])]
+    public function edit(string $id): Response
+    {
+        $response = $this->client->get("products/$id");
+
+        return $this->render('partner/layouts/product/edit.html.twig', [
+            'product' => $this->client->toArray($response)
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @throws Throwable
+     */
+    #[Route('/bulk', name: 'partner_product_bulk', methods: ['GET'])]
+    public function bulk(): Response
+    {
+        return $this->render('partner/layouts/product/edit.html.twig');
+    }
+
+    /**
+     * @return Response
+     * @throws Throwable
+     */
+    #[Route('/util', name: 'partner_product_util', methods: ['GET'])]
+    public function util(): Response
+    {
+        return $this->render('partner/layouts/product/edit.html.twig');
+    }
+
+}
