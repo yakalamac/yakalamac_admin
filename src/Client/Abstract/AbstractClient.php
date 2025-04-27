f6184aef8eb9791714adfd5aade9c7dc991dfadd
Onur Kudret <onurkudrt31@gmail.com>
2025-04-27 23:04:11 +0300

    refactor: new functions for easy control

--- src/Client/Abstract/AbstractClient.php
+++ src/Client/Abstract/AbstractClient.php
@@ -9,6 +9,7 @@ namespace App\Client\Abstract;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 use Throwable;
@@ -93,5 +94,62 @@ abstract class AbstractClient
             'error' => $result->getMessage(),
             'status' => $result->getCode()
         ];
+    }
+
+    /**
+     * @param ResponseInterface|Throwable $result
+     * @return bool
+     * @throws TransportExceptionInterface
+     */
+    public function isSuccess(ResponseInterface|Throwable $result): bool
+    {
+        if($result instanceof Throwable) {
+            return FALSE;
+        }
+
+        $status = $result->getStatusCode();
+
+        return $status > 199 && $status < 300;
+    }
+
+    /**
+     * @param ResponseInterface|Throwable $result
+     * @return array
+     * @throws Throwable
+     */
+    public function toResponseArray(ResponseInterface|Throwable $result): array
+    {
+        if($result instanceof ResponseInterface) {
+
+            $status = $result->getStatusCode();
+
+            return [
+                'status' => $status,
+                'content' => $status !== Response::HTTP_NO_CONTENT ? $result->toArray(false) : NULL,
+                'success' => $this->isSuccess($result),
+                'exception' => NULL
+            ];
+        }
+
+        return [
+            'success' => false,
+            'error' => $result->getMessage(),
+            'status' => $result->getCode(),
+            'exception' => $result::class
+        ];
+    }
+
+    /**
+     * @param array $array
+     * @return Response
+     */
+    public function toResponseFromArray(array $array): Response
+    {
+        if(isset($array['exception'])) {
+            unset($array['exception']);
+            return new JsonResponse(data: $array, status: 500);
+        }
+
+        return new JsonResponse(data: $array['content'], status: $array['status']);
     }
 }
