e99464114a077f12185dda06da7ce56ce032c284
Onur Kudret <onurkudrt31@gmail.com>
2025-05-15 00:53:51 +0300

    refactor: removed unused fields

--- src/Controller/Partner/Place/PlacePhotoController.php
+++ src/Controller/Partner/Place/PlacePhotoController.php
@@ -9,9 +9,11 @@ namespace App\Controller\Partner\Place;
 use App\Controller\Partner\Abstract\AbstractPartnerController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class PlacePhotoController extends AbstractPartnerController
 {
+    #[Route('/partner/photos', name: 'partner_photo', methods: ['GET'])]
     public function index(Request $request): Response
     {
         return $this->render('partner/layouts/video/index.html');
