7e822344bbb912112e250b71385d0aed04f73e12
Onur Kudret <onurkudrt31@gmail.com>
2025-05-29 10:29:17 +0300

    refactor: Controller methods documented

--- src/Controller/Public/LandingPageController.php
+++ src/Controller/Public/LandingPageController.php
@@ -7,25 +7,34 @@
 namespace App\Controller\Public;
 
 use App\Client\YakalaApiClient;
-use App\Security\Http\Abstract\Authenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Throwable;
 
 class LandingPageController extends AbstractController
 {
-    public function __construct(private YakalaApiClient $yakalaApiClient)
-    {
+    /**
+     * @param YakalaApiClient $yakalaApiClient
+     */
+    public function __construct(private readonly YakalaApiClient $yakalaApiClient) {}
 
-    }
-
+    /**
+     * @return Response
+     */
     #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('public/landing-page/index.html.twig');
     }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws Throwable
+     */
     #[Route('/appointment', name: 'appointment', methods: ['POST'])]
     public function appointment(Request $request): Response
     {
@@ -35,13 +44,14 @@ class LandingPageController extends AbstractController
                 'message' => 'Invalid request format.',
             ], Response::HTTP_BAD_REQUEST);
         }
+
         $data = json_decode($request->getContent(), true);
+
         $response = $this -> yakalaApiClient->post('appointments', [
-            'headers' => [
-                'content-type' => 'application/json'
-            ],
+            'headers' => ['content-type' => 'application/json'],
             'json' => $data
         ]);
+
         return $this->yakalaApiClient->toResponse($response);
     }
 }
