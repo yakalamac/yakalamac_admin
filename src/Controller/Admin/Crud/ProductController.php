2d243877e097bdbd7edd0cc5d5e015cc9853617e
bariskudret <bariskudret31@gmail.com>
2025-06-24 12:00:06 +0300

    fix: fixed routing for added product page

--- src/Controller/Admin/Crud/ProductController.php
+++ src/Controller/Admin/Crud/ProductController.php
@@ -47,7 +47,7 @@ class ProductController extends AbstractController
      * @return Response
      * @throws Throwable
      */
-    #[Route('/products/{id}', name: 'product_detail')]
+    #[Route('/products/{id}', name: 'product_detail', requirements: ['id' => '[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}'])]
     public function edit(string $id): Response
     {
         $result = $this->client->get("products/$id");
